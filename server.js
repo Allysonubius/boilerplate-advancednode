1b0b628b15405b883ffa2f9d5382f7fe73558c31
allyson <allyson.brandao@venhapranuvem.com.br>
2021-03-24 01:22:00 -0300

    Set up passport - Completed

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
+let session = require('express-session');
+let passport = require('passport');
+
 const app = express();
 
 app.set('view engine', 'pug');
@@ -13,6 +16,16 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route('/').get((req, res) => {
     res.render('pug/index', {
