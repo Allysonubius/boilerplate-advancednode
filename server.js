2ef3367c3184cd5802fc18daee9c57f9dffb40b4
allyson <allyson.brandao@venhapranuvem.com.br>
2021-03-24 03:46:50 -0300

    Authentication Strategies completed

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ let session = require('express-session');
 let passport = require('passport');
 
 const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require("passport-local");
 
 const app = express();
 
@@ -40,6 +41,20 @@ myDB(async(client) => {
             message: 'Please login'
         });
     });
+
+    // Authentication Strategies
+    passport.use(
+        new LocalStrategy((username, password, done) => {
+            db.collection("users").findOne({ username: username }, (err, user) => {
+                console.log("User " + username + " attempted to log in.");
+                if (err) return done(err);
+                if (!user) return done(null, false);
+                if (password !== user.password) return done(null, false);
+
+                return done(null, user);
+            });
+        })
+    );
 
     // Serialization and deserialization here...
     passport.serializeUser((user, done) => {
