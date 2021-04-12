a8d82f96e9e00f5292d05e7b2d5d262d3b6420cb
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2021-04-12 09:28:08 -0300

    Authentication with Socket.IOPassed Return question errror.

--- public/client.js
+++ public/client.js
@@ -1,22 +1,16 @@
-$(document).ready(function() {
-    /*global io*/
-    let socket = io();
+// This file's full path is /public/client.js
+$(document).ready(function () {
+  /*global io*/
+  let socket = io();
+  socket.on("user count", function (data) {
+    console.log(data);
+  });
+  // Form submittion with new message in field with id 'm'
 
-    socket.on('user', (data) => {
-        $('#num-users').text(data.currentUsers + ' users online');
-        let message = data.name + (data.connected ? ' has joined the chat.' : ' has left the chat.');
-        $('#messages').append($('<li>').html('<b>' + message + '</b>'));
-    });
-
-    socket.on('chat message', (data) => {
-        console.log('socket.on 1');
-        $('messsage').append($('<li>').text(`${data.name}: ${data.message}`));
-    });
-
-    $('form').submit(function() {
-        let messageToSend = $('#m').val();
-        socket.emit('chat message', messageToSend);
-        $('#m').val('');
-        return false;
-    });
+  $("form").submit(function () {
+    let messageToSend = $("#m").val();
+    socket.emit("chat message", messageToSend);
+    $("#m").val("");
+    return false;
+  });
 });
--- server.js
+++ server.js
@@ -1,97 +1,83 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const mongo = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const session = require('express-session');
-const passport = require('passport');
-const routes = require('./routes');
-const auth = require('./auth.js');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const MongoDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const passport = require("passport");
+const routes = require("./routes");
+const auth = require("./auth.js");
 
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const passportSocketIo = require('passport.socketio');
-const cookieParser = require('cookie-parser');
-const sessionStore = new session.MemoryStore();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const passportSocketIo = require("passport.socketio");
+const cookieParser = require("cookie-parser");
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 fccTesting(app); // For fCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-    secret: process.env.SESSION_SECRET,
+app.use(
+  session({
+    secret: process.env.MONGO_URI,
     resave: true,
     saveUninitialized: true,
     cookie: { secure: false },
-    key: 'express.sid',
-    store: sessionStore,
-}));
+    key: "express.sid"
+  })
+);
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: "exoress.sid",
+    secret: process.env.MONGO_URI,
+    success: onAuthorizeSucess,
+    fail: onAuthorizeFail
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-io.use(
-    passportSocketIo.authorize({
-        cookieParser: cookieParser,
-        key: 'express.sid',
-        secret: process.env.SESSION_SECRET,
-        //store: store,
-        success: onAuthorizeSuccess,
-        fail: onAuthorizeFail
-    })
-);
+MongoDB(async (client) => {
+  const myDataBase = await client.db("passport").collection("users");
 
-mongo(async(client) => {
-    const myDataBase = await client.db('passport').collection('users');
+  routes(app, myDataBase);
+  auth(app, myDataBase);
 
-    routes(app, myDataBase);
-    auth(app, myDataBase);
+  let currentUsers = 0;
+  io.on("connection", (socket) => {
+    ++currentUsers;
+    io.emit("user count", currentUsers);
+    console.log("A user has connected");
 
-    let currentUsers = 0;
-    io.on('connection', (socket) => {
-        ++currentUsers;
-        io.emit('user count', {
-            name: socket.request.user.name,
-            currentUsers,
-            connected: true
-        });
-        socket.on('chat message', (message) => {
-            io.emit('chat message', { name: socket.request.user.name, message });
-        });
-        console.log('A user has connected');
-        socket.on('disconnected', () => {
-            console.log('A user has disconnected');
-            --currentUsers;
-            io.emit('user', {
-                name: socket.request.user.name,
-                currentUsers,
-                connected: false
-            });
-        });
+    socket.on("disconnect", () => {
+      console.log("A user has disconnected");
+      --currentUsers;
+      io.emit("user count", currentUsers);
     });
+  });
 }).catch((e) => {
-    app.route('/').get((req, res) => {
-        res.render('pug', { title: e, message: 'Unable to login' });
-    });
+  app.route("/").get((req, res) => {
+    res.render("pug", { title: e, message: "Unable to login" });
+  });
 });
 
-function onAuthorizeSuccess(data, accept) {
-    console.log('successful connection to socket.io');
-
-    accept(null, true);
+function onAuthorizeSucess(data, accept) {
+  console.log("Sucessful connection to socket.io");
+  accept(null, true);
 }
 
 function onAuthorizeFail(data, message, error, accept) {
-    if (error) throw new Error(message);
-    console.log('failed connection to socket.io:', message);
-    accept(null, false);
+  if (error) throw new Error(message);
+  console.log("Failed connection to socket.io: ", message);
+  accept(null, false);
 }
 
 http.listen(process.env.PORT, () => {
-    console.log();
-    console.log('Open in web browser https://localhost:' + process.env.PORT);
+  console.log("Open in browser https://localhost:3000" + process.env.PORT);
 });
